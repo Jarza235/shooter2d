8551cdd25621c6a4a2ce9368596c6fb64e1cfec4
JiiTL <tlehtikallio@gmail.com>
2018-02-10 14:22:36 +0200

    Shooting logic added

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public bool isFiring;
+
+    public BulletController bullet;
+    public float bulletSpeed; // Declares the speed of the bullet
+
+    // Cooldown between shots
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    // Bullet spread 
+    public float BulletSpread; // Random spread in angles, e.g. 5 = -5 degrees .. +5 degrees
+    private float spreadZ;
+
+    public Transform firePoint; 
+
+    void Start ()
+    {
+		
+    }
+	
+	void Update ()
+    {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter <= 0) // Player can shoot again after shotCounter <= 0. timebetweenShots declares how long time you have to wait.
+            {
+                shotCounter = timeBetweenShots;
+                BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
+                spreadZ = Random.Range(-BulletSpread, BulletSpread); // Declares unique spread to every bullet
+                newBullet.transform.Rotate(0, 0, spreadZ); // Spread in action
+                newBullet.bulletSpeed = bulletSpeed; 
+            }
+        }
+        else
+        {
+            shotCounter = 0;
+        }
+        Debug.Log(shotCounter);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    private float speed = 0.2f;
+
+    public GunController theGun;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move player with WASD
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                //transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 315.0f);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                //transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 45.0f);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                //transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 225.0f);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                //transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 135.0f);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            theGun.isFiring = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            theGun.isFiring = false;
+        }
+    }
+}
