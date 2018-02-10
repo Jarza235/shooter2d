0e8da18329255465a0b4c9392d30729e8475db3d
Jari Räisänen <jari.raisanen@dodreams.com>
2018-02-10 15:57:47 +0200

    Door system added to house. The roof disappears when player walks in to the house

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour {
+
+	public GameObject roof;
+
+	/*void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collision");
+		if(collision.gameObject.tag.Equals("Player")) {
+			roof.SetActive(false);
+			Debug.Log("Collision with player and door");
+		}
+	}*/
+		
+	void OnTriggerEnter(Collider other) {
+		if(other.tag.Equals("Player")) {
+			if(roof.activeInHierarchy) {
+				roof.SetActive(false);
+			}
+			else {
+				roof.SetActive(true);
+			}
+		}
+	}
+
+}
