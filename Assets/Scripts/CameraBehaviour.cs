a8230d28d7e9efb5ce0053065b6f6c061165a10c
Jari Räisänen <jari.raisanen@dodreams.com>
2018-02-11 23:09:49 +0200

    Player rotated

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -14,7 +14,7 @@ public class CameraBehaviour : MonoBehaviour {
 	void Update () {
 		if(followPlayer) {
 			// Move the camera X & Y with the player
-			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+			transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10f, player.transform.position.z);
 		}
 		else if(followCar) {
 			transform.position = new Vector3(car.transform.position.x, car.transform.position.y + 10f, car.transform.position.z);
