46fa7dd56dda19c3925a9ebf80c137b5211cb9bb
JiiTL <tlehtikallio@gmail.com>
2018-03-12 23:51:39 +0200

    Grenade and Unity Particle Pack added.

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float explodeDelay = 2f;
+    private float countdown;
+
+    public GameObject explosionEffect;
+    public float blastRadius = 5f;
+    public float blastForce = 700f;
+
+    bool hasExploded = false;
+
+	void Start ()
+    {
+        countdown = explodeDelay;
+	}
+	
+	void Update ()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown < 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        // Show effect 
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        // Destroy destructible objects // No destructible objects yet
+        /*Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, blastRadius);
+        foreach (Collider nearbyObject in collidersToDestroy)
+        {
+            Destructible destroy = nearbyObject.GetComponent<Destructible>();
+            if (destroy != null)
+            {
+                destroy.Destroy();
+            }
+        }*/
+
+        // Add force to nearby undestructible objects
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, blastRadius);
+        foreach (Collider nearbyObject in collidersToMove)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if ( rb != null)
+            {
+                rb.AddExplosionForce(blastForce, transform.position, blastRadius);
+            }
+        }
+        
+        Destroy(gameObject); // Remove grenade
+    }
+}
---
+++ Assets/Scripts/ThrowGrenade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrenade : MonoBehaviour
+{
+    public float maxThrowForce;
+    public float throwForce;
+
+    public GameObject grenadePrefab;
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		if (Input.GetMouseButton(1))
+        {
+            if(throwForce < maxThrowForce)
+            {
+                throwForce += 0.1f;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            GrenadeThrow();
+            Debug.Log(throwForce);
+            throwForce = 5f;
+        }
+    }
+
+    void GrenadeThrow()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.right * throwForce, ForceMode.VelocityChange);
+        rb.AddForce(transform.forward * -2f, ForceMode.VelocityChange);
+        grenade.transform.Rotate(0f, 0f, 300f);
+    }
+}
