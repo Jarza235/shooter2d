1500a317012a05278b4b0a729dea84f61d11a91f
jarza <jjraisanen@hotmail.com>
2018-04-29 20:01:53 +0300

    Weapon looting added.

---
+++ Assets/Scripts/LootWeapons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootWeapons : MonoBehaviour
+{
+    private PlayerBehaviour PB;
+    private HealthBarAnimation HBA;
+    public GameObject weapon;
+
+    void Start ()
+    {
+        PB = GameObject.Find("Player3D").GetComponent<PlayerBehaviour>();
+        HBA = GameObject.Find("HealthBarRege").GetComponent<HealthBarAnimation>();
+    }
+	
+	/*void Update ()
+    {
+        
+    }*/
+
+    private void OnCollisionStay(Collision collideLootWeapon)
+    {
+        if(collideLootWeapon.gameObject.tag == "Player" && Input.GetKey(KeyCode.F))
+        {
+            weapon.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
